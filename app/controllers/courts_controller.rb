e1541d163fb306430372e8563e257108405788b2
Andre Pereira <andreporteira@gmail.com>
2022-06-17 12:45:42 -0300

    Devise e I18n

--- app/controllers/courts_controller.rb
+++ app/controllers/courts_controller.rb
@@ -1,5 +1,6 @@
 class CourtsController < ApplicationController
   before_action :set_court, only: %i[ show edit update destroy ]
+  before_action :authenticate_admin!
 
   # GET /courts or /courts.json
   def index
