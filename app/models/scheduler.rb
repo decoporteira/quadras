2849f2f396def0b057ebbc7a6e97b59fce8d4c13
Andre Pereira <andreporteira@gmail.com>
2022-06-20 22:01:19 -0300

    Alterações no model Scheduler - validate:

--- app/models/scheduler.rb
+++ app/models/scheduler.rb
@@ -3,4 +3,12 @@ class Scheduler < ApplicationRecord
   belongs_to :court
 
   enum status: { available: 0, unavailable: 1 }
+
+  validate :not_past_date
+
+  def not_past_date
+    if self.date < Date.today
+      errors.add(:date, "Não pode fazer reservar para datas passadas")
+    end
+  end
 end
