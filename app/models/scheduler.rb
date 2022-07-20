71b10acaafb847a4672828f4cff6b7a1637a94d2
Andre Pereira <andreporteira@gmail.com>
2022-07-20 14:54:11 -0300

    Validação pronta

--- app/models/scheduler.rb
+++ app/models/scheduler.rb
@@ -5,10 +5,18 @@ class Scheduler < ApplicationRecord
 
   enum status: { available: 0, unavailable: 1 }
   translate_enum :status
+  validates_presence_of :start_time, :end_time
+  validate :not_past_date, :court_available
 
-  validate :not_past_date
   #validates_uniqueness_of :court, scope: [:date]
-  #validate :not_same_hour
+
+  # Return a scope for all interval overlapping the given interval, excluding the given interval itself
+  #scope :overlapping, -> { where("id <> ? AND start_time <= ? AND ? <= end_time", scheduler.id, scheduler.end_time, scheduler.start_time) }
+  def court_available
+    if Scheduler.where(court_id: court_id, date: date, start_time: start_time...end_time).count > 0
+      errors.add(:time, " - Esse horário já está reservado, ou está com conflito com outro horário.")
+    end
+  end
 
   def not_past_date
     if date < Date.today
